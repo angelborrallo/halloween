fb3d6fbbca3c5200e90ef87f3747b29e3fbaf75b
angelborrallo <93764800+angelborrallo@users.noreply.github.com>
2021-11-14 17:52:12 +0100

    Add files via upload

---
+++ javascript/Halloween.js
@@ -0,0 +1,17 @@
+/******************************jacascript de la presentacion de halloween */
+
+function mostrar(imagen){
+    document.getElementById(imagen).style.visibility="visible"
+}
+
+function ocultar(imagen){
+    document.getElementById(imagen).style.visibility="hidden"
+}
+
+function sonar(sonido){
+    document.getElementById(sonido).play();
+}
+
+function pausar(sonido){
+    document.getElementById(sonido).pause();
+}
